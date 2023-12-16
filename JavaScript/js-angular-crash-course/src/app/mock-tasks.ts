3bf114c99bdb3a0f18c8c304b22afe1bb1b77d49
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-12-16 12:56:35 +0100

    Added tasks and task-item components.

---
+++ JavaScript/js-angular-crash-course/src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 0,
+        text: "Eating",
+        day: "May 5th at 12:00pm",
+        reminder: false
+    },
+    {
+        id: 1,
+        text: "Working",
+        day: "May 4th at 8:30am",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Meeting at school",
+        day: "May 6th at 1:30pm",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Food shopping",
+        day: "May 7th at 12:30pm",
+        reminder: false
+    }
+];
